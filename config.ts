f53021aa2d739bb283339d47af9889d09f9dcf4b
Alexey Khrushch <alexeykhr@outlook.com>
2021-06-06 00:33:50 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,41 @@
 import IConfig from './src/interfaces/IConfig';
 
 export default {
-  //
+  services: {
+    github: {
+      configuration: {
+        nickname: 'Alexeykhr',
+        sort: {
+          repositories: [
+            { attr: 'created_at', sortByDesc: true },
+            { attr: 'forks_count', sortByDesc: true },
+            { attr: 'stargazers_count', sortByDesc: true },
+          ],
+        },
+        filter: {
+          repositories: [
+            [
+              { attr: 'owner.login', values: ['Alexeykhr', 'GPortfolio', 'uSchedule', 'uRepairPC'] },
+              { attr: 'name', values: ['awesome-vue', 'merge-conflict', 'github-pages-with-jekyll', 'markdown-portfolio'], revert: true },
+              { attr: 'fork', values: false }
+            ],
+          ],
+        },
+      },
+    },
+  },
+  data: {
+    position: 'PHP Developer',
+    links: [
+      { name: 'github', url: 'https://github.com/alexeykhr' },
+      { name: 'linkedin', url: 'https://linkedin.com/in/alexeykhr' },
+    ],
+  },
+  templates: {
+    default: {
+      configuration: {
+        background: () => require('./public/background.jpg'),
+      },
+    },
+  },
 } as IConfig;
